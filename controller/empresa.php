1811228a1ca9ca8bc984007c177873272ac7db8c
Felipe <contatofemachado@gmail.com>
2023-06-20 14:36:59 -0300

    ech

--- controller/empresa.php
+++ controller/empresa.php
@@ -3,7 +3,7 @@ function readEmpresas()
 {
     require 'conexao.php';
     $sql = "SELECT id, empresa FROM empresa ORDER BY empresa ASC;";
-    //echo $sql;
+    echo $sql;
     $result = mysqli_query($conn, $sql);
     //echo $result;
     while ($row = mysqli_fetch_array($result)) :; ?>
