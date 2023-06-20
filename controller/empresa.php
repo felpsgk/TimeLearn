72ef4477c49688ce50e6939b8388ab89a383d39c
Felipe <contatofemachado@gmail.com>
2023-06-20 14:38:26 -0300

    echo retire

--- controller/empresa.php
+++ controller/empresa.php
@@ -3,7 +3,7 @@ function readEmpresas()
 {
     require 'conexao.php';
     $sql = "SELECT id, nome FROM empresa ORDER BY nome ASC;";
-    echo $sql;
+    //echo $sql;
     $result = mysqli_query($conn, $sql);
     //echo $result;
     while ($row = mysqli_fetch_array($result)) :; ?>
