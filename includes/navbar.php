bb66802f98d58891860636e0a468d85bac5b0120
Felipe <contatofemachado@gmail.com>
2023-06-20 15:31:36 -0300

    nav

--- includes/navbar.php
+++ includes/navbar.php
@@ -9,10 +9,7 @@
                 <li class="nav-item"><a class="nav-link" href="topicos.php">Tópicos</a></li>
                 <li class="nav-item"><a class="nav-link" href="#">Consultores</a></li>
                 <li class="nav-item"><a class="nav-link" href="contato.php">Contato</a></li>
-                <li class="nav-item"><a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
-                        <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
-                        Logout
-                    </a></li>
+                <li class="nav-item"><a class="nav-link" href="controller/logoffUser.php"><i class="bi bi-box-arrow-right"></i></a></li>
             </ul>
         </div>
     </div>
