0cbf08e699aea3928f51f05f1d16649463ca3ae0
Felipe <contatofemachado@gmail.com>
2023-06-20 14:41:03 -0300

    a

--- controller/empresa.php
+++ controller/empresa.php
@@ -5,7 +5,7 @@ function readEmpresas()
     $sql = "SELECT id, nome FROM empresa ORDER BY nome ASC;";
     //echo $sql;
     $result = mysqli_query($conn, $sql);
-    //echo $result;
+    echo $result;
     while ($row = mysqli_fetch_array($result)) :; ?>
 
         <option id="empresa" value="<?php echo $row['id'] ?>"><?php echo $row['empresa'] ?></option>
