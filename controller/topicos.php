a51e3493ce2b72515926a360e0e98e07de4d81ff
Felipe <contatofemachado@gmail.com>
2023-06-23 19:13:29 -0300

    col card

--- controller/topicos.php
+++ controller/topicos.php
@@ -12,12 +12,16 @@ function readTopicos()
             <div class="card overflow-hidden shadow rounded-4 border-0 mb-5">
                 <div class="card-body p-0">
                     <div class="d-flex align-items-center">
-                        <div class="p-5">
-                            <h2 class="fw-bolder"><?php echo $row['nome'] ?></h2>
-                            <p class="text-break"><?php echo $row['descricao'] ?></p>
-                            <a style="background: linear-gradient(to right, #1e30f3, #6f42c1);  color: #fff;  width: 100%;  padding: 10px 20px;  border: none;  border-radius: 5px;  cursor: pointer;" onmouseover="this.style.background ='linear-gradient(to right, #6f42c1, #1e30f3)';" onmouseout="this.style.background ='linear-gradient(to right, #1e30f3, #6f42c1)';" class="btn" role="button" href="agendas.php">Buscar agenda</a>
+                        <div class="row">
+                            <div class="col-md-6 p-5">
+                                <h2 class="fw-bolder"><?php echo $row['nome'] ?></h2>
+                                <p class="text-break"><?php echo $row['descricao'] ?></p>
+                                <a style="background: linear-gradient(to right, #1e30f3, #6f42c1);  color: #fff;  width: 100%;  padding: 10px 20px;  border: none;  border-radius: 5px;  cursor: pointer;" onmouseover="this.style.background ='linear-gradient(to right, #6f42c1, #1e30f3)';" onmouseout="this.style.background ='linear-gradient(to right, #1e30f3, #6f42c1)';" class="btn" role="button" href="agendas.php">Buscar agenda</a>
+                            </div>
+                            <div class="col-md-6">
+                                <img class="img-fluid" style="width: 300px; height: 400px;" src="<?php echo $row['imgsrc'] ?>" alt="..." />
+                            </div>
                         </div>
-                        <img class="img-fluid" style="width: 300px; height: 400px;" src="<?php echo $row['imgsrc'] ?>" alt="..." />
                     </div>
                 </div>
             </div>
