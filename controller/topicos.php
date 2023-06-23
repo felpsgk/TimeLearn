a44da03c0729aa0c47d3c0f1c90ee248d83db960
Felipe <contatofemachado@gmail.com>
2023-06-23 14:50:35 -0300

    topicos start

---
+++ controller/topicos.php
@@ -0,0 +1,24 @@
+<?php
+function readTopicos()
+{
+    require 'conexao.php';
+    $sql = "SELECT id, nome, descricao FROM topicos ORDER BY nome ASC;";
+    //echo $sql;
+    $result = mysqli_query($conn, $sql);
+    //echo $result;
+    while ($row = mysqli_fetch_array($result)) :; ?>
+        <div class="card overflow-hidden shadow rounded-4 border-0 mb-5">
+            <div class="card-body p-0">
+                <div class="d-flex align-items-center">
+                    <div class="p-5">
+                        <h2 class="fw-bolder"><?php echo $row['nome'] ?></h2>
+                        <p><?php echo $row['descricao'] ?></p>
+                    </div>
+                    <img class="img-fluid" src="<?php echo $row['imgsrc'] ?>" alt="..." />
+                </div>
+            </div>
+        </div>
+        
+<?php endwhile;
+}
+?>
--- topicos.php
+++ topicos.php
@@ -1,7 +1,7 @@
 <?php
 //session_start();
 session_start();
-if(!$_SESSION['usuario']){
+if (!$_SESSION['usuario']) {
     header('Location: login.php');
     exit();
 }
@@ -46,30 +46,9 @@ include 'includes/head.php'
                 </div>
                 <div class="row gx-5 justify-content-center">
                     <div class="col-lg-11 col-xl-9 col-xxl-8">
-                        <!-- Project Card 1-->
-                        <div class="card overflow-hidden shadow rounded-4 border-0 mb-5">
-                            <div class="card-body p-0">
-                                <div class="d-flex align-items-center">
-                                    <div class="p-5">
-                                        <h2 class="fw-bolder">Project Name 1</h2>
-                                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius at enim eum illum aperiam placeat esse? Mollitia omnis minima saepe recusandae libero, iste ad asperiores! Explicabo commodi quo itaque! Ipsam!</p>
-                                    </div>
-                                    <img class="img-fluid" src="https://dummyimage.com/300x400/343a40/6c757d" alt="..." />
-                                </div>
-                            </div>
-                        </div>
-                        <!-- Project Card 2-->
-                        <div class="card overflow-hidden shadow rounded-4 border-0">
-                            <div class="card-body p-0">
-                                <div class="d-flex align-items-center">
-                                    <div class="p-5">
-                                        <h2 class="fw-bolder">Project Name 2</h2>
-                                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius at enim eum illum aperiam placeat esse? Mollitia omnis minima saepe recusandae libero, iste ad asperiores! Explicabo commodi quo itaque! Ipsam!</p>
-                                    </div>
-                                    <img class="img-fluid" src="https://dummyimage.com/300x400/343a40/6c757d" alt="..." />
-                                </div>
-                            </div>
-                        </div>
+                        <?php
+                        readTopicos();
+                        ?>
                     </div>
                 </div>
             </div>
